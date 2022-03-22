3d6a71bd592544898841dd1b6e8cd2c612f25b38
t-i-0414 <gutlo.ol0414@gmail.com>
2022-03-22 21:11:34 +0900

    645-medium-diff

---
+++ @types/Diff.d.ts
@@ -0,0 +1,45 @@
+/*
+  645 - Diff
+  -------
+  by ZYSzys (@ZYSzys) #medium #object
+
+  ### Question
+
+  Get an `Object` that is the difference between `O` & `O1`
+
+  > View on GitHub: https://tsch.js.org/645
+*/
+
+/* _____________ Your Code Here _____________ */
+
+// your answers
+export type Diff<O, O1> = keyof O extends keyof O1
+  ? { [K in Exclude<keyof O1, keyof O>]: O1[K] }
+  : { [K in Exclude<keyof O, keyof O1>]: O[K] };
+
+type test = Diff<Foo, Bar>;
+
+/* _____________ Test Cases _____________ */
+import { Equal, Expect } from "@type-challenges/utils";
+
+type Foo = {
+  name: string;
+  age: string;
+};
+type Bar = {
+  name: string;
+  age: string;
+  gender: number;
+};
+
+type cases = [
+  Expect<Equal<Diff<Foo, Bar>, { gender: number }>>,
+  Expect<Equal<Diff<Bar, Foo>, { gender: number }>>
+];
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/645/answer
+  > View solutions: https://tsch.js.org/645/solutions
+  > More Challenges: https://tsch.js.org
+*/
